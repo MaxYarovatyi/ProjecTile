0b7b7b657d13d604edf31e34368c7f47d70b15d7
makcyarovatij@gmail.com <makcyarovatij@gmail.com>
2024-09-23 22:17:16 +0300

    Added account components and configured client main files

--- API/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9d653ddc4914904cafae0b12bab5646f12920c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890fdcb053f4b83e5a2def6fd0af51d0c1d99442")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
